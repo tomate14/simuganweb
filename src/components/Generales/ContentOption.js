001989e83f3909f07180eea9476b89fddabeaadb
DESKTOP-5DSFGHD\MAXI <maxiroselli@gmail.com>
2018-08-18 21:07:55 -0300

    Cantidad de inputs con variacion dinamica

--- src/components/Generales/ContentOption.js
+++ src/components/Generales/ContentOption.js
@@ -9,10 +9,18 @@ class ContentOption extends Component {
 	constructor(props){
 		super(props);
 		this.estaHabilitado = true;
+		this.state = props.state;
+		this.handleOnChangeValue = this.handleOnChangeValue.bind(this);
 	};
 
+	handleOnChangeValue(e){
+		this.setState({
+			cantidadVariaciones  :e.target.value
+		});
+	}
 	render(){
 		//Relleno los months de un vector de datos 
+		console.log(this.state.cantidadVariaciones);
 		return (
 				<div>
 					<div>
@@ -20,7 +28,7 @@ class ContentOption extends Component {
 						<h5> Permitir el uso de este parametro para variar</h5>
 					</div>
 					<div>
-						<input type="number" className="number" id="cantVariation" value="1"/>
+						<input type="number" className="checkbox" id="cantVariation" onChange={this.handleOnChangeValue} value={this.state.cantidadVariaciones}/>
 						<h5> Cantidad de variaciones a realizar</h5>
 					</div>
 				</div>
--- src/components/Generales/InputsVariation.js
+++ src/components/Generales/InputsVariation.js
@@ -4,6 +4,38 @@ import PropTypes from 'prop-types';
 //Estilos del componente
 import './css/InputsVariation.css';
 
-class InputVariation extends Component{
-	
+class InputsVariation extends Component{
+	constructor(props){
+		super(props);
+		this.state = props.state;
+		this.handleInputValueChange = this.handleInputValueChange.bind(this);
+		
+	}
+	setInputsVariations(){
+		let inputs = [];
+		for( let i= 0; i < this.state.cantidadVariaciones; i++){
+		    inputs.push({name:'<input type="number" />'});
+		};
+		return inputs;
+	}
+
+	handleInputValueChange(idx){
+
+	}
+	render(){
+		let inputs = this.setInputsVariations();
+		return(
+			<div className="divInputsVariations">
+				{inputs.map((input, idx) => (
+		            <input
+		              type="number"
+		              className="InputVariables"
+		              id = {idx}
+		              onChange={this.handleInputValueChange(idx)}
+		            />
+		        ))}
+			</div>
+		);
+	}
 }
+export default InputsVariation;
