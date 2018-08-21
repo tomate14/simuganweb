e4bae7294ebd45a0a822e108926b649ad5c97942
tomate14 <maxiroselli@gmail.com>
2018-08-21 11:27:28 -0300

    Add files via upload
    Formato bootstrap

--- src/components/RecursosForrajeros/index.js
+++ src/components/RecursosForrajeros/index.js
@@ -7,16 +7,33 @@ class RecursosForrajeros extends Component {
 	constructor(){
 		super();
 		this.state = {
-			cantidadVariaciones : 15
+			cantidadVariaciones : 12
 		}
 	}
 	render(){
-		return(
-			<div>
+		/* <div>
 				<ContentOption state={this.state}/>
 				<MonthTable />
 				<InputsVariation state={this.state} />
 			</div>
+			*/
+		return(
+			<div className="conteiner">
+				<div className="row">
+	                <div className="col-sm-12">
+	                    <ContentOption state={this.state}/>
+	                </div>
+	            </div>
+	            <div className="row">
+	                <div className="col-sm-4">
+	                    <MonthTable />
+	                </div>
+	                <div className="col-sm-8">
+	                    <InputsVariation state={this.state} />
+	                </div>
+	            </div>
+	        </div>
+			
 		);
 	}
 }
