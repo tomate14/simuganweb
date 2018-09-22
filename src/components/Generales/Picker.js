fecd9379230d78c22c89fdcd6483c770dbc5ca31
DESKTOP-5DSFGHD\MAXI <maxiroselli@gmail.com>
2018-09-22 14:59:34 -0300

    Dropdown en el componente picker.

---
+++ src/components/Generales/Picker.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {Row, Col,Dropdown, DropdownToggle,DropdownItem,DropdownMenu } from 'reactstrap';
+
+import {bindActionCreators} from 'redux';
+
+
+class Picker extends Component {
+
+	constructor(){
+		super();
+		this.state = {dropdownOpen: false}			
+	    this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
+		this.setState(prevState => ({ dropdownOpen: !prevState.dropdownOpen}) );
+	}
+
+	setDropdownSelected(){
+	  	//este arreglo se debe cargar desde el xml parseado
+	  	  	
+	 	let aux =this.props.nombres[this.props.dropDownSelected];
+	  	return aux;
+	}
+
+	render(){
+		return (
+			<Dropdown  isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+		        <DropdownToggle color = "dark" caret>
+		        	 {this.setDropdownSelected()}
+		        </DropdownToggle>
+		        <DropdownMenu>
+		          	<DropdownItem onClick={this.props.funcSelected} header>Pasturas</DropdownItem>
+		          	{
+		          		this.props.nombres.map((object,i)=>{
+		          												return <DropdownItem 
+		          												                onClick = {this.props.funcSelected} 
+		          												                key = {i} 
+		          												                id = {i}>{object}
+		          												       </DropdownItem>;
+		          											}
+		          							  )
+		          }
+		        </DropdownMenu>
+	      	</Dropdown>
+		);
+	}
+}
+
+export default Picker;
